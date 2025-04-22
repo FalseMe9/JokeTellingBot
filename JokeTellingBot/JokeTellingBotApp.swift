7bb05c97f208282f8223de87338500af7b450578
Billie <150131870+FalseMe9@users.noreply.github.com>
2025-04-22 22:36:31 +0700

    Initial Commit

---
+++ JokeTellingBot/JokeTellingBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeTellingBotApp.swift
+//  JokeTellingBot
+//
+//  Created by Billie H on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct JokeTellingBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
